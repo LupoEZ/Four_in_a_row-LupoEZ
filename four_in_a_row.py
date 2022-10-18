a67e46ae90ba888ea72ae828d1f38895c9020ec5
LupoEZ <115219094+LupoEZ@users.noreply.github.com>
2022-10-18 14:24:52 +0200

    Brainstorming complete

---
+++ four_in_a_row.py
@@ -0,0 +1,20 @@
+#class for the player
+#name, p1 or p2, token
+#method to place token
+#method to win, loose, or get a draw (track wins of a player)
+
+
+#class for the game (fields of the game, columns and rows)
+#dictionary for the columns related to the rows, so i dont need 7 or 6 lists
+#method to create/clear game field (numerize the columns underneath or above)
+#method to edit a field on the game
+#method to check if somebody won the game
+
+
+
+#input for preparation of the game (player names, their token)
+#prepare the game, create field
+
+#gameplay - input to place tokens, way of naming the different columns and showing these names, so players know where to place tokens
+
+#game end - Someone won, loose or a draw - Question if players wanna play another game (tracking wins of one player)
