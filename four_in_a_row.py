fdd9168c691bf335a2641b6496548eccb791dfdc
LupoEZ <115219094+LupoEZ@users.noreply.github.com>
2022-10-21 22:13:00 +0200

    Added the player inputs to place tokens, a check if they can't place it and method to edit the field

--- four_in_a_row.py
+++ four_in_a_row.py
@@ -1,46 +1,62 @@
-#method to place token
-#method to win, loose, or get a draw (track wins of a player)
+#class for the Player 1 and 2
 class Player():
         win_counter = 0
         token = "E"
-
+        
         def __init__(self, name, player_number):
             self.name = name
             self.player_number = player_number
-        
+        #message for start of the game
         def __repr__(self):
             return "{name} is playing as Player {number} with the \"{token}\" token.".format(name=self.name, number=self.player_number, token=self.token)
         
+            
 
-#method to create/clear game field (numerize the columns underneath or above)
+        
+
 #method to edit a field on the game
 #method to check if somebody won the game
 class Four_In_A_Row():
+        #dictionary for the single fields of the game
+        # fields[row] = list[columns of that row]
         fields = {}
-        for i in range (6):
-            fields[i] = list(range(7))
+        for i in range (7):
+            fields[i] = list(range(6))
 
         def __repr__(self):
             return "Welcome to Four In A Row! Let's have some fun in a game with many (4,531,985,219,092) possibilities. Before we can start you need to enter your names and choose a token. "
 
+        #method to create a game field or clear the last for the next game
         def create_clear_game(self):
-            for j in range(6):
-                for i in range(7):
+            for j in range(7):
+                for i in range(6):
                     self.fields[j][i] = "-"
-            for j in range(6):
+            self.print_field()
+        
+        def print_field(self):
+            for i in range(6):
                 row = ""
-                for i in range(7):
+                for j in range(7):
                     row += ("{field}".format(field=self.fields[j][i]))
                     row += "|"
                 row = row[:-1]
                 print(row)
-
+            print("1 2 3 4 5 6 7")
+        
+        def edit_field(self, column_number, player_token):
+            for i in range(6):
+                if self.fields[column_number - 1][-i - 1] == "-":
+                    self.fields[column_number - 1][-i - 1] = player_token
+                    break
+            self.print_field()
+                
 
 
 #initialize the game
 game = Four_In_A_Row()
 print(game)
-#player preparations
+
+#player input name and token
 player1 = Player(input("Hello there! What is your name Player 1?: "), 1)
 player2 = Player(input("Greetings Player 2. What is your name?: "), 2)
 token1 = input("{name}. Because you are Player 1, u can decide with which token u want to play. Wanna play as \"X\" or as \"O\"?: ".format(name=player1.name))
@@ -51,11 +67,29 @@ if token1 == "X":
 else:
     player2.token = "X"
 player1.token = token1
+
 #game preparation
 game.create_clear_game()
 print(player1)
 print(player2)
 
-#gameplay - input to place tokens, way of naming the different columns and showing these names, so players know where to place tokens
-
+#gameplay - player input to place their tokens
+move = 0
+random_number = 1
+while move < 42:
+    move += 1
+    if move % 2 == 1:
+        current_player_name = player1.name
+        current_player_token = player1.token
+    else:
+        current_player_name = player2.name
+        current_player_token = player2.token
+    column_number = int(input("{name} it`s your turn! In which column do you wanna place ur \"{token}\"? ".format(name=current_player_name, token=current_player_token)))
+    #check if the column is filled already and infinite loop, if they dont pick an empty column
+    while random_number < 10:
+        if game.fields[column_number - 1][0] != "-":
+            column_number = int(input("I'm sorry {name}, but that column is already completely filled. Take another one to place our \"{token}\". ".format(name=current_player_name, token=current_player_token)))
+        else:
+            break
+    game.edit_field(column_number, current_player_token)
 #game end - Someone won, loose or a draw - Question if players wanna play another game (tracking wins of one player)
